3a9dd0f4101a59fe71c055536d40e239b80fcbbd
YevheniiaShen <128001989+YevheniiaShen@users.noreply.github.com>
2023-09-01 19:57:25 +0200

    com

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,23 @@
+const form = document.querySelector('.login-form');
 
+form.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(event) {
+
+    event.preventDefault();
+    
+    const formElements = event.currentTarget.elements;
+    const email = formElements.email.value;
+    const password = formElements.password.value;
+    const formData = {};
+    
+if (email === '' || password === '') {
+       return  alert("All input fields must be filled!!!")
+    };
+    
+    formData.email = email;
+    formData.password = password;
+    console.log(formData);
+    form.reset();
+    
+}
--- js/task-09.js
+++ js/task-09.js
@@ -1,5 +1,16 @@
+const btnChangeColor = document.querySelector('.change-color');
+const spanColor = document.querySelector('.color');
+const body = document.body;
+
+btnChangeColor.addEventListener('click', changeColor);
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
-    .padStart(6, 0)}`;
-}
+    .padStart(6,0)}`;
+};
+
+function changeColor(event) {
+  body.style.backgroundColor = getRandomHexColor();
+  spanColor.textContent = body.style.backgroundColor;
+};
