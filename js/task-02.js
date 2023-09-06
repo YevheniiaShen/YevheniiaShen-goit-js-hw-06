880f50de7ac5da6297a00c6104901360eb242d5a
YevheniiaShen <128001989+YevheniiaShen@users.noreply.github.com>
2023-09-06 21:23:20 +0200

    task 2 fix

--- js/task-02.js
+++ js/task-02.js
@@ -1,18 +1,19 @@
-
 const ingredients = [
-  "Potatoes",
-  "Mushrooms",
-  "Garlic",
-  "Tomatos",
-  "Herbs",
-  "Condiments",
+  'Potatoes',
+  'Mushrooms',
+  'Garlic',
+  'Tomatos',
+  'Herbs',
+  'Condiments',
 ];
 
-const ul = document.getElementById("ingredients");
+const ulEl = document.querySelector('#ingredients');
 
-ingredients.forEach((ingredient) => {
-  const li = document.createElement("li");
-  li.textContent = ingredient;
-  li.classList.add("item");
-  ul.appendChild(li);
+const filter = ingredients.map((ingredient) => {
+  const listEl = document.createElement('li');
+  listEl.classList.add('item');
+  listEl.textContent = ingredient;
+  return listEl;
 });
+
+ulEl.append(...filter);
